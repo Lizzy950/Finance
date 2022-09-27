b5d963fc122f447b8b9fd835ceb17a0a81d9747e
Liz Odukoya <liz95.od@gmail.com>
2022-09-27 19:31:45 +0100

    Added Portfolio Valuation file

---
+++ Portfolio Valuation_II.py
@@ -0,0 +1,35 @@
+#Portfolio Valuation
+
+import datetime
+import pandas as pd
+import pandas_datareader as pdr
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+import numpy as np
+
+df = pdr.get_data_yahoo(['AAPL', 'GE', 'GOOG', 'IBM', 'ADM','ABF','AAL'], 
+                               start=datetime.datetime(2012, 7, 10), 
+                               end=datetime.datetime(2013, 7, 10))['Adj Close']
+
+returns = df / df.shift(1) - 1  # to obtain the returns of the stock
+
+mean = returns.mean() # average of returns
+
+VarianceCo = df.cov() # Variance Covariance of returns
+
+Components = np.array([0.143, 0.143, 0.143, 0.143, 0.143, 0.143, 0.143])
+
+TransComponent = Components.transpose()
+
+PortReturn = np.dot(TransComponent, mean)
+
+ # Portfolio Return
+
+PortVariance = np.dot(np.dot(TransComponent, VarianceCo),TransComponent)
+
+ #Portfolio Variance
+
+PortVolatility = PortVariance ** 0.5
+
+print(PortReturn, PortVariance, PortVolatility)
